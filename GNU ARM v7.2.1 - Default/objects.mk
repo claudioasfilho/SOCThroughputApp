7ae5c7b14465449d2ce7033b9c4db9833060faaa
Claudio Filho <claudioasfilho@gmail.com>
2019-08-20 17:28:51 -0700

    Coex library and configurations added

--- GNU ARM v7.2.1 - Default/objects.mk
+++ GNU ARM v7.2.1 - Default/objects.mk
@@ -2,7 +2,7 @@
 # Automatically-generated file. Do not edit!
 ################################################################################
 
-USER_OBJS := /Users/clfilho/SimplicityStudio/Apr19_Workspace/SOCThroughputApp/protocol/bluetooth/lib/EFR32BG13P/GCC/binapploader.o /Users/clfilho/SimplicityStudio/Apr19_Workspace/SOCThroughputApp/protocol/bluetooth/lib/EFR32BG13P/GCC/libpsstore.a /Users/clfilho/SimplicityStudio/Apr19_Workspace/SOCThroughputApp/protocol/bluetooth/lib/EFR32BG13P/GCC/libbluetooth.a /Users/clfilho/SimplicityStudio/Apr19_Workspace/SOCThroughputApp/platform/radio/rail_lib/autogen/librail_release/librail_efr32xg13_gcc_release.a /Users/clfilho/SimplicityStudio/Apr19_Workspace/SOCThroughputApp/protocol/bluetooth/lib/EFR32BG13P/GCC/libmbedtls.a
+USER_OBJS := /Users/clfilho/SimplicityStudio/Apr19_Workspace/SOCThroughputApp/protocol/bluetooth/lib/EFR32BG13P/GCC/binapploader.o /Users/clfilho/SimplicityStudio/Apr19_Workspace/SOCThroughputApp/protocol/bluetooth/lib/EFR32BG13P/GCC/libcoex.a /Users/clfilho/SimplicityStudio/Apr19_Workspace/SOCThroughputApp/protocol/bluetooth/lib/EFR32BG13P/GCC/libpsstore.a /Users/clfilho/SimplicityStudio/Apr19_Workspace/SOCThroughputApp/protocol/bluetooth/lib/EFR32BG13P/GCC/libbluetooth.a /Users/clfilho/SimplicityStudio/Apr19_Workspace/SOCThroughputApp/platform/radio/rail_lib/autogen/librail_release/librail_efr32xg13_gcc_release.a /Users/clfilho/SimplicityStudio/Apr19_Workspace/SOCThroughputApp/protocol/bluetooth/lib/EFR32BG13P/GCC/libmbedtls.a
 
 LIBS := -lm
 
--- hal-config.h
+++ hal-config.h
@@ -7,7 +7,39 @@
 
 #define HAL_VCOM_ENABLE                   (0)
 #define HAL_I2CSENSOR_ENABLE              (0)
-#define HAL_SPIDISPLAY_ENABLE             (1)
+#define HAL_SPIDISPLAY_ENABLE             (0)
 #define HAL_SPIDISPLAY_FREQUENCY                      (1000000)
+
+
+// $[COEX]
+#define HAL_COEX_ENABLE                    (1)
+
+#define BSP_COEX_REQ_PIN                   (10)
+#define BSP_COEX_REQ_PORT                  (gpioPortC)
+#define BSP_COEX_REQ_ASSERT_LEVEL          (1)
+#define HAL_COEX_REQ_SHARED                (0)
+#define HAL_COEX_REQ_BACKOFF               (15)
+#define HAL_COEX_REQ_WINDOW                (50+15)
+
+#define BSP_COEX_GNT_PIN                   (3)
+#define BSP_COEX_GNT_PORT                  (gpioPortF)
+#define BSP_COEX_GNT_ASSERT_LEVEL          (0)
+#define HAL_COEX_TX_ABORT                  (0)
+
+#define BSP_COEX_PRI_PIN                   (12)
+#define BSP_COEX_PRI_PORT                  (gpioPortD)
+#define BSP_COEX_PRI_ASSERT_LEVEL          (1)
+#define HAL_COEX_PRI_SHARED                (0)
+
+#define HAL_COEX_RX_HIPRI                  (1)
+#define HAL_COEX_TX_HIPRI                  (1)
+
+
+#define BSP_COEX_RHO_PIN                   (11)
+#define BSP_COEX_RHO_PORT                  (gpioPortC)
+#define BSP_COEX_RHO_ASSERT_LEVEL          (0)
+
+// [COEX]$
+
 
 #endif
